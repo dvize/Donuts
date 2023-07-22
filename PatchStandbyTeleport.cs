7f84f5bf242c6b1dc32c107b3884b1b63e4de006
dvize <dvize404@gmail.com>
2023-07-21 17:47:43 -0700

    Apply patch for Standby Logic

---
+++ PatchStandbyTeleport.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Aki.Reflection.Patching;
+using Aki.Reflection.Utils;
+using EFT;
+using System.Threading.Tasks;
+using EFT.AssetsManager;
+using System.Reflection;
+using HarmonyLib;
+
+namespace dvize.Donuts
+{
+    internal class PatchStandbyTeleport : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            // Method used by SPT for finding BaseLocalGame
+            return AccessTools.Method(typeof(GClass351), "UpdateNode");
+        }
+
+        [PatchPrefix]
+        public static bool Prefix(GClass351 __instance, BotStandByType ___botStandByType_0)
+        {
+            FieldInfo botOwnerField = AccessTools.Field(typeof(GClass294), "botOwner_0");
+
+            if (botOwnerField != null)
+            {
+                BotOwner botOwner = botOwnerField.GetValue(__instance) as BotOwner;
+
+                if (!botOwner.Settings.FileSettings.Mind.CAN_STAND_BY)
+                {
+                    return false;
+                }
+            }
+
+            if (!__instance.CanDoStandBy)
+            {
+                return false;
+            }
+
+            if (___botStandByType_0 == BotStandByType.goToSave)
+            {
+                MethodInfo method1 = AccessTools.Method(typeof(GClass351), "method_1");
+                if (method1 != null)
+                {
+                    method1.Invoke(__instance, new object[] { });
+                }
+            }
+
+            return false;
+        }
+    }
+}
