6b626e009bd74dd377d82bdf9a970ae920ff5a05
dvize <dvize404@gmail.com>
2024-03-31 13:50:16 -0700

    Fix Duplicate Folders

---
+++ Patches/BotProfilePreparationHook.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using Aki.Reflection.Patching;
+using EFT;
+
+namespace Donuts
+{
+    internal class BotProfilePreparationHook : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod() => typeof(BotsController).GetMethod(nameof(BotsController.AddActivePLayer));
+
+        [PatchPrefix]
+        public static void PatchPrefix() => DonutsBotPrep.Enable();
+    }
+}
---
+++ Patches/MatchEndPlayerDisposePatch.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Aki.Reflection.Patching;
+using EFT;
+using EFT.AssetsManager;
+using HarmonyLib;
+using UnityEngine;
+
+namespace Donuts
+{
+    internal class MatchEndPlayerDisposePatch : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            // Method used by SPT for finding BaseLocalGame
+            return AccessTools.Method(typeof(BaseLocalGame<GamePlayerOwner>), nameof(BaseLocalGame<GamePlayerOwner>.smethod_4));
+        }
+
+        [PatchPrefix]
+        private static bool PatchPrefix(IDictionary<string, Player> players)
+        {
+            foreach (Player player in players.Values)
+            {
+                if (player != null)
+                {
+                    try
+                    {
+                        player.Dispose();
+                        AssetPoolObject.ReturnToPool(player.gameObject, true);
+                    }
+                    catch (Exception ex)
+                    {
+                        Debug.LogException(ex);
+                    }
+                }
+            }
+            players.Clear();
+
+            return false;
+        }
+    }
+}
