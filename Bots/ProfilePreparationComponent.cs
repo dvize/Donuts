423730d05f4a17303cf27856979074d63a72efc3
dvize <dvize404@gmail.com>
2024-06-25 07:15:18 -0700

    Remove event handlers when the bots are removed for goal enemy changed. also clean up ondestroy method in profile preparation component.

--- Bots/ProfilePreparationComponent.cs
+++ Bots/ProfilePreparationComponent.cs
@@ -107,6 +107,7 @@ namespace Donuts
 
             botSpawnerClass.OnBotRemoved += (BotOwner bot) =>
             {
+                bot.Memory.OnGoalEnemyChanged -= Memory_OnGoalEnemyChanged;
                 OriginalBotSpawnTypes.Remove(bot.Profile.Id);
             };
 
@@ -413,14 +414,15 @@ namespace Donuts
             {
                 botSpawnerClass.OnBotRemoved -= (BotOwner bot) =>
                 {
+                    bot.Memory.OnGoalEnemyChanged -= Memory_OnGoalEnemyChanged;
                     OriginalBotSpawnTypes.Remove(bot.Profile.Id);
                 };
-            }
 
-            botSpawnerClass.OnBotCreated -= (BotOwner bot) =>
-            {
-                bot.Memory.OnGoalEnemyChanged -= Memory_OnGoalEnemyChanged;
-            };
+                botSpawnerClass.OnBotCreated -= (BotOwner bot) =>
+                {
+                    bot.Memory.OnGoalEnemyChanged -= Memory_OnGoalEnemyChanged;
+                };
+            }
 
             if (mainplayer != null)
             {
