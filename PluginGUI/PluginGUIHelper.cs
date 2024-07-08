197fb123ebed48172ce7af49fbaa9bc8b0b437b6
dvize <dvize404@gmail.com>
2024-07-08 11:31:21 -0700

    Fix Donuts Config Title Displaying Uncentered

--- PluginGUI/PluginGUIHelper.cs
+++ PluginGUI/PluginGUIHelper.cs
@@ -46,7 +46,7 @@ namespace Donuts
                 // Save the current GUI skin
                 GUISkin originalSkin = GUI.skin;
 
-                windowRect = GUI.Window(123, windowRect, MainWindowFunc, "Donuts Configuration", windowStyle);
+                windowRect = GUI.Window(123, windowRect, MainWindowFunc, "", windowStyle);
                 GUI.FocusWindow(123);
 
                 if (Event.current.type == EventType.MouseDown || Event.current.type == EventType.MouseUp || Event.current.type == EventType.MouseDrag)
@@ -178,6 +178,17 @@ namespace Donuts
 
         private void MainWindowFunc(int windowID)
         {
+
+            // Manually draw the window title centered at the top
+            Rect titleRect = new Rect(0, 0, windowRect.width, 20);
+            GUI.Label(titleRect, "Donuts Configuration", new GUIStyle(GUI.skin.label)
+            {
+                alignment = TextAnchor.MiddleCenter,
+                fontSize = 20,
+                fontStyle = FontStyle.Bold,
+                normal = { textColor = Color.white }
+            });
+
             GUILayout.BeginVertical();
 
             scrollPosition = GUILayout.BeginScrollView(scrollPosition, GUILayout.ExpandHeight(true));
