273971473138976acda0e561c8d014fcf2266776
p-kossa <kossa526+git@gmail.com>
2024-08-21 12:41:20 -0400

    logs

--- Bots/BotSpawn.cs
+++ Bots/BotSpawn.cs
@@ -24,31 +24,26 @@ namespace Donuts
             get; private set;
         }
 
-        public DonutBotSpawn()
-        {
-            Logger ??= BepInEx.Logging.Logger.CreateLogSource(nameof(DonutBotSpawn));
-        }
-
         internal static async UniTask SpawnBotsFromInfo(ConcurrentBag<BotSpawnInfo> botSpawnInfos, CancellationToken cancellationToken)
         {
             if (botSpawnInfos == null)
             {
-                Debug.LogError("SpawnBotsFromInfo: botSpawnInfos list is null.");
+                DonutComponent.Logger.LogError("SpawnBotsFromInfo: botSpawnInfos list is null.");
                 return;
             }
 
             if (!botSpawnInfos.Any())
             {
-                Debug.LogError("SpawnBotsFromInfo: botSpawnInfos list is empty.");
+                DonutComponent.Logger.LogError("SpawnBotsFromInfo: botSpawnInfos list is empty.");
                 return;
             }
 
-            Debug.Log("SpawnBotsFromInfo: Starting spawn tasks for botSpawnInfos.");
+            DonutComponent.Logger.LogInfo("SpawnBotsFromInfo: Starting spawn tasks for botSpawnInfos.");
             var spawnTasks = botSpawnInfos.Select(botSpawnInfo =>
             {
                 if (botSpawnInfo == null)
                 {
-                    Debug.LogError("SpawnBotsFromInfo: botSpawnInfo is null in the list.");
+                    DonutComponent.Logger.LogError("SpawnBotsFromInfo: botSpawnInfo is null in the list.");
                     return UniTask.CompletedTask;
                 }
                 return SpawnStartingBots(botSpawnInfo, cancellationToken);
@@ -59,41 +54,41 @@ namespace Donuts
 
         internal static async UniTask SpawnStartingBots(BotSpawnInfo botSpawnInfo, CancellationToken cancellationToken)
         {
-            Debug.Log("SpawnStartingBots: Entering method.");
+            DonutComponent.Logger.LogDebug("SpawnStartingBots: Entering method.");
 
             if (botSpawnInfo == null)
             {
-                Debug.LogError("SpawnStartingBots: BotSpawnInfo is null. Cannot proceed with spawning bots.");
+                DonutComponent.Logger.LogError("SpawnStartingBots: BotSpawnInfo is null. Cannot proceed with spawning bots.");
                 return;
             }
             if (botSpawnInfo.BotType == null)
             {
-                Debug.LogError("SpawnStartingBots: BotType is null. Cannot proceed with spawning bots.");
+                DonutComponent.Logger.LogError("SpawnStartingBots: BotType is null. Cannot proceed with spawning bots.");
                 return;
             }
             if (botSpawnInfo.Faction == null)
             {
-                Debug.LogError("SpawnStartingBots: Faction is null. Cannot proceed with spawning bots.");
+                DonutComponent.Logger.LogError("SpawnStartingBots: Faction is null. Cannot proceed with spawning bots.");
                 return;
             }
             if (botSpawnInfo.GroupSize == null)
             {
-                Debug.LogError("SpawnStartingBots: GroupSize is null. Cannot proceed with spawning bots.");
+                DonutComponent.Logger.LogError("SpawnStartingBots: GroupSize is null. Cannot proceed with spawning bots.");
                 return;
             }
             if (botSpawnInfo.Coordinates == null || !botSpawnInfo.Coordinates.Any())
             {
-                Debug.LogError("SpawnStartingBots: Coordinates list is null or empty. Cannot proceed with spawning bots.");
+                DonutComponent.Logger.LogError("SpawnStartingBots: Coordinates list is null or empty. Cannot proceed with spawning bots.");
                 return;
             }
             if (botSpawnInfo.Difficulty == null)
             {
-                Debug.LogError("SpawnStartingBots: Difficulty is null. Cannot proceed with spawning bots.");
+                DonutComponent.Logger.LogError("SpawnStartingBots: Difficulty is null. Cannot proceed with spawning bots.");
                 return;
             }
             if (string.IsNullOrEmpty(botSpawnInfo.Zone))
             {
-                Debug.LogError("SpawnStartingBots: Zone is null or empty. Cannot proceed with spawning bots.");
+                DonutComponent.Logger.LogError("SpawnStartingBots: Zone is null or empty. Cannot proceed with spawning bots.");
                 return;
             }
 
@@ -104,19 +99,19 @@ namespace Donuts
             var botDifficulty = botSpawnInfo.Difficulty;
             var zone = botSpawnInfo.Zone;
 
-            Debug.Log($"SpawnStartingBots: WildSpawnType: {wildSpawnType}, Side: {side}, GroupSize: {groupSize}, BotDifficulty: {botDifficulty}, Zone: {zone}");
+            DonutComponent.Logger.LogInfo($"SpawnStartingBots: WildSpawnType: {wildSpawnType}, Side: {side}, GroupSize: {groupSize}, BotDifficulty: {botDifficulty}, Zone: {zone}");
 
             var cachedBotGroup = DonutsBotPrep.FindCachedBots(wildSpawnType, botDifficulty, groupSize);
             if (cachedBotGroup == null)
             {
-                Debug.Log($"SpawnStartingBots: No starting bots found in cache for this spawn, generating data on the fly, this may take some time.");
+                DonutComponent.Logger.LogInfo($"SpawnStartingBots: No starting bots found in cache for this spawn, generating data on the fly, this may take some time.");
                 var botInfo = new PrepBotInfo(wildSpawnType, botDifficulty, side, groupSize > 1, groupSize);
                 await DonutsBotPrep.CreateBot(botInfo, botInfo.IsGroup, botInfo.GroupSize, cancellationToken);
                 DonutsBotPrep.BotInfos.Add(botInfo);
                 cachedBotGroup = botInfo.Bots;
             }
 
-            Debug.LogWarning("SpawnStartingBots: Starting bot group found in cache, proceeding with spawn checks.");
+            DonutComponent.Logger.LogWarning("SpawnStartingBots: Starting bot group found in cache, proceeding with spawn checks.");
 
             var minSpawnDistFromPlayer = SpawnChecks.GetMinDistanceFromPlayer();
 
@@ -124,7 +119,7 @@ namespace Donuts
             {
                 if (WildSpawnTypeDictionaries.IsBoss(wildSpawnType) || WildSpawnTypeDictionaries.IsFollower(wildSpawnType))
                 {
-                    Debug.Log($"SpawnStartingBots: Boss or Follower bot detected, skipping spawn checks.");
+                    DonutComponent.Logger.LogInfo($"SpawnStartingBots: Boss or Follower bot detected, skipping spawn checks.");
                     await BotSpawnHelper.ActivateStartingBots(cachedBotGroup, wildSpawnType, side, botCreator, botSpawnerClass, coordinate, botDifficulty, groupSize, zone, cancellationToken);
                     return;
                 }
@@ -137,62 +132,62 @@ namespace Donuts
                 }
             }
 
-            Debug.Log($"SpawnStartingBots: All coordinates in zone {zone} failed for this spawn, skipping this spawn");
+            DonutComponent.Logger.LogInfo($"SpawnStartingBots: All coordinates in zone {zone} failed for this spawn, skipping this spawn");
         }
 
         internal static async UniTask SpawnBots(BotWave botWave, string zone, Vector3 coordinate, string wildSpawnType, List<Vector3> coordinates, CancellationToken cancellationToken)
         {
-            Debug.Log("Entering SpawnBots method.");
+            DonutComponent.Logger.LogInfo("Entering SpawnBots method.");
 
             if (botWave == null)
             {
-                Debug.LogError("SpawnBots: botWave is null. Cannot proceed with spawning bots.");
+                DonutComponent.Logger.LogError("SpawnBots: botWave is null. Cannot proceed with spawning bots.");
                 return;
             }
 
             if (string.IsNullOrEmpty(zone))
             {
-                Debug.LogError("SpawnBots: Zone is null or empty. Cannot proceed with spawning bots.");
+                DonutComponent.Logger.LogError("SpawnBots: Zone is null or empty. Cannot proceed with spawning bots.");
                 return;
             }
 
             if (coordinates == null || !coordinates.Any())
             {
-                Debug.LogError("SpawnBots: Coordinates list is null or empty. Cannot proceed with spawning bots.");
+                DonutComponent.Logger.LogError("SpawnBots: Coordinates list is null or empty. Cannot proceed with spawning bots.");
                 return;
             }
 
             if (string.IsNullOrEmpty(wildSpawnType))
             {
-                Debug.LogError("SpawnBots: wildSpawnType is null or empty. Cannot proceed with spawning bots.");
+                DonutComponent.Logger.LogError("SpawnBots: wildSpawnType is null or empty. Cannot proceed with spawning bots.");
                 return;
             }
 
             WildSpawnType actualWildSpawnType = BotSpawnHelper.DetermineWildSpawnType(wildSpawnType);
             int maxCount = BotSpawnHelper.DetermineMaxBotCount(wildSpawnType, botWave.MinGroupSize, botWave.MaxGroupSize);
 
-            Debug.Log($"SpawnBots: WildSpawnType: {actualWildSpawnType}, MaxCount: {maxCount}, Zone: {zone}, Coordinate: {coordinate}");
+            DonutComponent.Logger.LogInfo($"SpawnBots: WildSpawnType: {actualWildSpawnType}, MaxCount: {maxCount}, Zone: {zone}, Coordinate: {coordinate}");
 
             if (HardCapEnabled.Value)
             {
-                Debug.Log("SpawnBots: HardCap is enabled. Adjusting max count based on active bot counts.");
+                DonutComponent.Logger.LogInfo("SpawnBots: HardCap is enabled. Adjusting max count based on active bot counts.");
                 maxCount = await BotSpawnHelper.AdjustMaxCountForHardCap(wildSpawnType, maxCount, cancellationToken);
             }
 
             if (cancellationToken.IsCancellationRequested)
             {
-                Debug.Log("SpawnBots: Cancellation requested. Exiting SpawnBots method.");
+                DonutComponent.Logger.LogInfo("SpawnBots: Cancellation requested. Exiting SpawnBots method.");
                 return;
             }
 
             if (maxCount == 0)
             {
-                Debug.Log("SpawnBots: Max bot count is 0, skipping spawn");
+                DonutComponent.Logger.LogInfo("SpawnBots: Max bot count is 0, skipping spawn");
                 return;
             }
 
             bool isGroup = maxCount > 1;
-            Debug.Log($"SpawnBots: Setup spawn for {(isGroup ? "group" : "single")} with maxCount: {maxCount}");
+            DonutComponent.Logger.LogInfo($"SpawnBots: Setup spawn for {(isGroup ? "group" : "single")} with maxCount: {maxCount}");
 
             try
             {
@@ -200,15 +195,15 @@ namespace Donuts
             }
             catch (Exception ex)
             {
-                Debug.LogError($"SpawnBots: Error during SetupSpawn: {ex.Message}\n{ex.StackTrace}");
+                DonutComponent.Logger.LogError($"SpawnBots: Error during SetupSpawn: {ex.Message}\n{ex.StackTrace}");
             }
 
-            Debug.Log("Exiting SpawnBots method.");
+            DonutComponent.Logger.LogInfo("Exiting SpawnBots method.");
         }
 
         private static void LogCancellation(CancellationToken cancellationToken)
         {
-            cancellationToken.Register(() => Debug.Log("LogCancellation: Cancellation token was triggered."));
+            DonutComponent.Logger.LogInfo(() => Debug.Log("LogCancellation: Cancellation token was triggered."));
         }
     }
 }
